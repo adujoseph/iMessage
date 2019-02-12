1ff8a412f22cdd5bb374cc144403fda68ea43081
adujoseph <iamjosephadu@gmail.com>
2019-02-12 09:22:06 +0100

    Initial Commit

---
+++ iMessenger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iMessenger
+//
+//  Created by MAC on 12/02/2019.
+//  Copyright © 2019 MAC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
